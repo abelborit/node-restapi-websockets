632a951165fbcf74e5b3ab9addc04917211cc34c
Abel Borit <abelboritguitton@gmail.com>
2024-11-13 15:34:33 -0500

    [add] view - new ticket

--- public/js/new-ticket.js
+++ public/js/new-ticket.js
@@ -1,1 +1,24 @@
-console.log("Nuevo Ticket HTML");
+const currentTicketLbl = document.querySelector("span");
+const createTicketBtn = document.querySelector("button");
+
+const getLastTicket = async () => {
+  const lastTicket = await fetch("/api/ticket/last").then((response) =>
+    response.json()
+  );
+  // console.log({ lastTicket });
+
+  currentTicketLbl.innerText = lastTicket;
+};
+
+const createTicket = async () => {
+  const newTicket = await fetch("/api/ticket/create", { method: "POST" }).then(
+    (response) => response.json()
+  );
+  // console.log(newTicket);
+
+  currentTicketLbl.innerText = newTicket.number;
+};
+
+createTicketBtn.addEventListener("click", createTicket);
+
+getLastTicket();

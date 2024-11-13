ec24686d20f5d5ce03a217a8077f1d1ce285b31a
Abel Borit <abelboritguitton@gmail.com>
2024-11-13 14:32:12 -0500

    [add] use TicketService in TicketController

--- src/presentation/tickets/controller.ts
+++ src/presentation/tickets/controller.ts
@@ -1,34 +1,39 @@
 import { Request, Response } from "express";
+import { TicketService } from "../services/ticket.service";
 
 export class TicketController {
   /* aquí haremos la inyección de dependencias de nuestro "WebSocketServerService" porque nuestro retfull API usará información de ese servicio para poder mandar comunicación por websockets a través del restfull API */
-  constructor() {}
+  constructor(private readonly ticketService = new TicketService()) {}
 
   public getAllTickets = async (request: Request, response: Response) => {
-    response.json("getTickets");
+    response.status(200).json(this.ticketService.tickets);
   };
 
   public getLastTicket = async (request: Request, response: Response) => {
-    response.json("getLastTicket");
+    response.status(200).json(this.ticketService.lastTicket);
   };
 
   public pendingTickets = async (request: Request, response: Response) => {
-    response.json("pendingTickets");
+    response.status(200).json(this.ticketService.pendingTickets);
   };
 
   public workingOnTicket = async (request: Request, response: Response) => {
-    response.json("workingOnTicket");
+    response.status(200).json(this.ticketService.lastWorkingOnTickets);
   };
 
   public createTicket = async (request: Request, response: Response) => {
-    response.json("createTicket");
+    response.status(201).json(this.ticketService.createTicket());
   };
 
   public drawTicket = async (request: Request, response: Response) => {
-    response.json("drawTicket");
+    const { desk } = request.params;
+
+    response.status(200).json(this.ticketService.drawTicket(desk));
   };
 
   public doneTicket = async (request: Request, response: Response) => {
-    response.json("doneTicket");
+    const { ticketId } = request.params;
+
+    response.status(200).json(this.ticketService.finishedTicket(ticketId));
   };
 }
